4eaad00af853bcba8874d433c890f571f177f8e2
mrrugby <snjishaka@gmail.com>
2026-05-11 12:46:30 +0300

    added new sitemap and robot.txt

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import generateSitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+
+    generateSitemap({
+      hostname: 'https://technav.store',
+    }),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
